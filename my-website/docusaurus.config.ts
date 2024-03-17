fec637bd75f731cc8e7d018be6b6a37ea8249649
AUnterrainer <unterrainer.alexander@gmail.com>
2024-03-17 11:10:39 +0000

    Typo in gihub username in config

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -12,10 +12,10 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'DecfonQ', // Usually your GitHub org/user name.
+  organizationName: 'DefconQ', // Usually your GitHub org/user name.
   projectName: 'defconQ', // Usually your repo name.
 
   onBrokenLinks: 'throw',
